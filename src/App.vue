d23edd7360b82f037ccfde8ecf3dd97967bfd737
Frank Laumann Nielsen <franklaumann@hotmail.com>
2021-02-26 13:45:43 +0100

    Navbar component created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
-    <img alt="Vue logo" src="">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Navbar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Navbar
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<!-- markup -->
+<template>
+  <nav class="navbar">
+    <div class="navbar__logo">
+        <a href="#">Title and logo</a>
+    </div>
+
+    <div class="navbar__menu">
+        Put navbar menu here
+    </div>
+    <div class="navbar__search">
+        <a href="#">Search</a>
+    </div>
+</nav>
+</template>
+
+<!-- Script -->
+<script>
+    export default {
+        name: 'Navbar',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Styles -->
+<style lang="scss" scoped>
+    .navbar {
+        background-color: lightgray;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 20px;
+    }
+</style>
